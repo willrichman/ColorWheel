f940429f5b3ab7cc60621cee67ce87f60977d260
Will Richman <wrichman@gmail.com>
2015-10-22 18:37:15 -0700

    Initial Commit

---
+++ ColorWheel WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  ColorWheel WatchKit Extension
+//
+//  Created by William Richman on 10/22/15.
+//  Copyright © 2015 Will Richman. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
